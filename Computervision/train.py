e26a7a248a316c1155136ef1d849ecb2ed190f18
HaiNguyen <146414635+Hainguyen752004@users.noreply.github.com>
2025-04-08 19:14:11 +0700

    Delete Computervision/train.py

--- Computervision/train.py
+++
@@ -1,8 +0,0 @@
-from ultralytics import YOLO
-
-def train_model():
-    model = YOLO("yolov8n.pt")  
-    model.train(data="data.yaml", epochs=100, imgsz=640, device=0)
-  
-if __name__ == '__main__':
-    train_model()  
